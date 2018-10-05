0307e98d5f6965146bb6f1883689747378eb263c
YamamotoKazuma2018 <e1q14098@oit.ac.jp>
2018-10-05 16:12:10 +0900

    A

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,6 +1,10 @@
 class TalksController < ApplicationController
     protect_from_forgery with: :null_session
 
+    def index
+
+    end
+
     def create
       request = AlexaRubykit::build_request(params)
       response = AlexaRubykit::Response.new
